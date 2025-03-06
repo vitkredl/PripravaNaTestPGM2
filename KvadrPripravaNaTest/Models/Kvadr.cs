c984e5c1730f340617ee66747743f4d1315213ba
vitkr <vitkr@VITPC>
2025-03-06 11:11:37 +0100

    Add project files.

---
+++ KvadrPripravaNaTest/Models/Kvadr.cs
@@ -0,0 +1,16 @@
+﻿namespace KvadrPripravaNaTest.Models
+{
+    public class Kvadr
+    {
+        private int stranaA;
+        private int stranaB;
+        private int stranaC;
+
+        public int StranaA { get => stranaA; set => stranaA = value; }
+        public int StranaB { get => stranaB; set => stranaB = value; }
+        public int StranaC { get => stranaC; set => stranaC = value; }
+
+        public double PovrchKvadru => 2 * (StranaA * StranaB + StranaA * StranaC + StranaB * StranaC);
+        public double ObjemKvadru => StranaA * StranaB * StranaC;
+    }
+}
